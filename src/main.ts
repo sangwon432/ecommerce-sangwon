540f7f6a1916c3d1439fd8cf5f4022e11757b869
sangwon432 <swonl0622@gmail.com>
2024-02-03 19:41:59 -0600

    Validator & Transformer

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { BaseAPIDoc } from './config/swagger.document';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+
+  // validator
+  app.useGlobalPipes(new ValidationPipe());
 
   // api doc setting with swagger
   const config = new BaseAPIDoc().initializeOptions();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,27 @@
 import { Provider } from '../entities/provider.enum';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2, { message: '최소 두 자리 이상' })
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString({ message: '문자열만 허용' })
+  @MinLength(8)
+  //최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자 :
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/, {
+    message: '최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자',
+  })
   password?: string;
   provider?: Provider;
   profileImg?: string;
