f9c674c468ee371c8952738eec58d99cb4d74974
sangwon432 <swonl0622@gmail.com>
2024-02-07 17:46:35 -0600

    create-blog-dto validator추가

--- src/blog/dto/create-blog.dto.ts
+++ src/blog/dto/create-blog.dto.ts
@@ -1,5 +1,18 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateBlogDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2, {message: '최소 두 글자 이상'})
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2, {message: '최수 두 글자 이상'})
   desc: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2, {message: '최소 두 글자 이상'})
   category: string;
 }
